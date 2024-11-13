db77c8d2cfd0b73fce3270c1f1d42601ac0624a1
geek <elmainiaymane03@gmail.com>
2024-11-13 17:10:01 +0100

    add logging to the service layer

--- src/main/java/com/progressoft/technicaltest/service/DealServiceImpl.java
+++ src/main/java/com/progressoft/technicaltest/service/DealServiceImpl.java
@@ -7,21 +7,29 @@ import com.progressoft.technicaltest.exception.DuplicateDealIdException;
 import com.progressoft.technicaltest.mapper.DealMapper;
 import com.progressoft.technicaltest.repository.DealRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class DealServiceImpl implements DealService {
     private final DealRepository dealRepository;
     private final DealMapper dealMapper;
 
     @Override
     public DealResponseDto save(DealRequestDto dto) {
-        if (dealRepository.existsById(dto.id()))
+        log.info("Attempting to save deal with ID: {}", dto.id());
+
+        if (dealRepository.existsById(dto.id())) {
+            log.warn("Duplicate deal ID detected: {}. Operation aborted.", dto.id());
             throw new DuplicateDealIdException("Deal id already exists");
+        }
 
         Deal savedDeal = dealRepository.save(dealMapper.toEntity(dto));
+        log.info("Deal saved successfully with ID: {}", savedDeal.getId());
+
         return dealMapper.toResponseEntity(savedDeal);
     }
 }
